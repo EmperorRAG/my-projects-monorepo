72db2bd4ad937af7e5f75364063dbfc3f08df87e
EmperorRAG <mrbrewermj@gmail.com>
2025-10-03 23:16:38 +0200

    Update TypeScript configuration to enable declaration files and maps

--- libs/utilities/rollup.config.cjs
+++ libs/utilities/rollup.config.cjs
@@ -3,23 +3,19 @@ const { withNx } = require('@nx/rollup/with-nx');
 module.exports = withNx(
 	{
 		main: './src/index.ts',
-		outputPath: 'libs/utilities/dist', // Use a relative path without './' to avoid path issues
+		outputPath: '../../dist/libs/utilities',
 		tsConfig: './tsconfig.lib.json',
 		compiler: 'swc',
 		format: ['esm'],
-		buildLibsFromSource: true,
-		useLegacyTypescriptPlugin: true,
 		allowJs: true,
+		buildLibsFromSource: true,
 		deleteOutputPath: true,
 		generatePackageJson: false,
 		generateExportsField: false,
 		sourceMap: false,
+		useLegacyTypescriptPlugin: false,
 	},
 	{
 		// Provide additional rollup configuration here. See: https://rollupjs.org/configuration-options
-		output: {
-			sourcemap: false,
-			//sourcemapBaseUrl: path.resolve(__dirname, '../../').replace(/\\/g, '/') + '/',
-		},
 	}
 );
